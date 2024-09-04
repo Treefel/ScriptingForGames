2067b54e491690eaaa887bac34269db6c8efcd26
Kade Buttars <buttarsfk@gmail.com>
2024-09-04 15:46:57 -0600

    Readded GameObjectsAndTransformLab
    Last unity project was not the correct version of Unity, fixed it.

--- GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
+++ GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
@@ -6,12 +6,10 @@ public class TransformController : MonoBehaviour
 {
     private void Update()
     {
-        // Move the target GameObject
-        var x = Mathf.PingPong(Time.time,3);
+        var x = Mathf.PingPong(Time.time, 3);
         var p = new Vector3(0, x, 0);
         transform.position = p;
 
-        //rotate the target GameObject
         transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
     }
 }
