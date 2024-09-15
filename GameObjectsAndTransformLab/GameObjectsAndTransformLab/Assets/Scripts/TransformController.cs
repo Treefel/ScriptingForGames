7688925f390f2eb50a661bcf4b27780204da7e64
Treefel <buttarsfk@gmail.com>
2024-09-15 14:40:56 -0600

    Updated GameObjectsAndTransform
    updated the Transform controller to now move side to side, rotate faster, and also rotate around the z axis.

--- GameObjectsAndTransformLab/GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
+++ GameObjectsAndTransformLab/GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
@@ -7,9 +7,9 @@ public class TransformController : MonoBehaviour
     void Update()
     {
         var x = Mathf.PingPong(Time.time, 3);
-        var p = new Vector3(0, x, 0);
+        var p = new Vector3(x, 0, 0);
         transform.position = p;
 
-        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 0, 70) * Time.deltaTime);
     }
 }
