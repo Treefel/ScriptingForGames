65cfd460fc780e047007dd189311bfad761da5b2
Treefel <buttarsfk@gmail.com>
2024-11-11 19:18:20 -0700

    Added Coroutine Particle Effect

--- AdventureGame/Assets/Scripts/TriggerParticleEffect.cs
+++ AdventureGame/Assets/Scripts/TriggerParticleEffect.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 [RequireComponent(typeof(ParticleSystem), typeof(Collider))]
 public class TriggerParticleEffect : MonoBehaviour
 {
-    private ParticleSystem particleSystem;
-    public int particleAmount = 10;
+    public ParticleSystem particleSystem;
+
+    public int firstEmissionAmount =  10;
+    public int secondEmissionAmount = 20;
+    public int thirdEmissionAmount = 30;
+    public float delayBetweenEmissions = 0.5f;
+
     private void Start()
     {
         particleSystem = GetComponent<ParticleSystem>();
@@ -16,7 +21,18 @@ public class TriggerParticleEffect : MonoBehaviour
     {
         if (other.gameObject.GetComponent<CharacterController>())
         {
-            particleSystem.Emit(particleAmount);
+            StartCoroutine(EmitParticlesCoroutine());
         }
+    }
+
+    private IEnumerator EmitParticlesCoroutine()
+    {
+        particleSystem.Emit(firstEmissionAmount);
+        yield return new WaitForSeconds(delayBetweenEmissions);
+        
+        particleSystem.Emit(secondEmissionAmount);
+        yield return new WaitForSeconds(delayBetweenEmissions);
+
+        particleSystem.Emit(thirdEmissionAmount);
     }
 }
